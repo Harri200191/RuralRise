4b46df74a039515287feca68610b9658699ba613
aimasibtain <146006397+aimasibtain@users.noreply.github.com>
2024-12-14 19:55:39 +0500

    Update index.tsx

--- client/src/pages/Tutorials/index.tsx
+++ client/src/pages/Tutorials/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { TutorialCard } from './TutorialCard';
 import type { TutorialType } from '../../types';
+import { TutorialCard } from './TutorialCard';
 
 export function Tutorials() {
   const tutorials: TutorialType[] = [
@@ -11,7 +10,8 @@ export function Tutorials() {
       image: 'https://images.unsplash.com/photo-1516321318423-f06f85e504b3?auto=format&fit=crop&q=80&w=500',
       category: 'E-commerce',
       duration: '2 hours',
-      lessons: 8
+      lessons: 8,
+      link: 'https://www.coursera.org/learn/foundations-of-digital-marketing-and-e-commerce?msockid=0b90f7273dad609518f1e5103ca461f5'
     },
     {
       id: '2',
@@ -20,7 +20,8 @@ export function Tutorials() {
       image: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&q=80&w=500',
       category: 'Marketing',
       duration: '1.5 hours',
-      lessons: 6
+      lessons: 6,
+      link:'https://www.bing.com/search?q=Social+Media+Marketing+coursera&cvid=cdd20254a6514a79a526100289bd39bd&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQg1MTA0ajBqOagCALACAA&FORM=ANAB01&PC=DCTS'
     },
     {
       id: '3',
@@ -29,7 +30,8 @@ export function Tutorials() {
       image: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&q=80&w=500',
       category: 'Finance',
       duration: '3 hours',
-      lessons: 10
+      lessons: 10,
+      link:'https://www.coursera.org/specializations/wharton-business-financial-modeling'
     }
   ];
 
