7812b58f138c543e5f922ebced0758f4afd02a75
haris rehman <harisrehmanchugtai@gmail.com>
2024-12-16 00:36:49 +0500

    Added relevant configs in backend and frontend

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://ruralrise-frontend.onrender.com"],
 }));
 
 app.use(express.json());
