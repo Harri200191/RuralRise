926292a2e302549834de1d85a13fb26fd1cb5bc1
haris rehman <harisrehmanchugtai@gmail.com>
2024-12-16 01:01:24 +0500

    Resolved cors issue

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://ruralrise-frontend.onrender.com"],
+  origin: ["http://localhost:5173", "https://ruralrise.onrender.com"],
 }));
 
 app.use(express.json());
