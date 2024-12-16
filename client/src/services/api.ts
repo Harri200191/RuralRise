672f066cc92d453c195509fe32584cc5f173251b
haris rehman <harisrehmanchugtai@gmail.com>
2024-12-16 13:21:10 +0500

    read from render if production env is set up

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
-const API_URL = 'https://ruralrise-backend.onrender.com/api';
+const DEV_ENV = process.env.DEV_ENV;
+let API_URL = 'http://localhost:5000/api';
+
+if (DEV_ENV == "development") {
+  API_URL = 'http://localhost:5000/api';
+} else {
+  if (DEV_ENV == "production") {
+    API_URL = 'https://ruralrise-backend.onrender.com/api';
+  }
+};
 
 const api = axios.create({
   baseURL: API_URL, 
